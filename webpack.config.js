9051bdf3e6b36513009be47364688ec397871d70
Yasminn Vaz <yasminnvaz@gmail.com>
2020-12-28 20:36:08 -0300

    adds loaders

--- webpack.config.js
+++ webpack.config.js
@@ -19,4 +19,20 @@ module.exports = {
             template: path.resolve(__dirname, "./src/index.html"),
         })
     ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [{ loader: 'file-loader', },],
+            },
+        ],
+    },
 }
